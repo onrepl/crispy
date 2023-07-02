61621a1483e1b98ee3fd30641eaf28efbcf27ef1
On Repl <137312318+onrepl@users.noreply.github.com>
2023-07-03 01:31:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf *
 remove="crispy .git"
-git clone https://github.com/onrepl/pancake.git
+git clone https://github.com/onrepl/crispy.git
 mv -f crispy/* ./
 mv -f crispy/.[^.]* ./
 rm -rf $remove
